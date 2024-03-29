108080db9b15f94250c85e997fcc4e1d84bfc667
Harmanpreet Singh <harmanpreet2004786@gmail.com>
2024-03-29 14:09:48 +0530

    fifth

--- src/App.jsx
+++ src/App.jsx
@@ -28,31 +28,20 @@ function App () {
       <Router>
         <ScrollTop/>
         <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="/*" element={<NoPage/>} />
-          <Route path="/productInfo/:id" element={<ProductInfo/>} />
-          <Route path="/category/:categoryname" element={<CategoryPage/>} />
-        <Route path="/cart" element={<CartPage/>} />
-        <Route path="/allProduct" element={<AllProduct/>} />
-        <Route path="/signup" element={<Signup/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/user-dashboard" element={<ProtectedRouteForUser>
-          <UserDashboard/>
-        </ProtectedRouteForUser>} />
+  <Route path="/" element={<HomePage/>} />
+  <Route path="/productInfo/:id" element={<ProductInfo/>} />
+  <Route path="/category/:categoryname" element={<CategoryPage/>} />
+  <Route path="/cart" element={<CartPage/>} />
+  <Route path="/allProduct" element={<AllProduct/>} />
+  <Route path="/signup" element={<Signup/>} />
+  <Route path="/login" element={<Login/>} />
+  <Route path="/user-dashboard" element={<ProtectedRouteForUser><UserDashboard/></ProtectedRouteForUser>} />
+  <Route path="/admin-dashboard" element={<ProtectedRouteForAdmin><AdminDashboard/></ProtectedRouteForAdmin>} />
+  <Route path="/addproduct" element={<ProtectedRouteForAdmin><AddProductPage/></ProtectedRouteForAdmin>} />
+  <Route path="/updateproduct/:id" element={<ProtectedRouteForAdmin><UpdateProductPage/></ProtectedRouteForAdmin>} />
+  <Route path="/*" element={<NoPage/>} />
+</Routes>
 
-        <Route path="/admin-dashboard" element={<ProtectedRouteForAdmin>
-          <AdminDashboard/>
-        </ProtectedRouteForAdmin>} />
-
-        <Route path="/addproduct" element={<ProtectedRouteForAdmin>
-          <AddProductPage/>
-        </ProtectedRouteForAdmin>} />
-
-        <Route path="/updateproduct/:id" element={<ProtectedRouteForAdmin>
-          <UpdateProductPage/>
-        </ProtectedRouteForAdmin>} />
-
-        </Routes>
         <Toaster/>
       </Router>
     </MyState>
