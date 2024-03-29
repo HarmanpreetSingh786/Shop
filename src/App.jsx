cfa0ac8a923456fbc1b6e4a4f689783e1b42d40e
Harmanpreet Singh <harmanpreet2004786@gmail.com>
2024-03-29 13:59:30 +0530

    fourth

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import NoPage from "./pages/noPage/NoPage";
+import ProductInfo from "./pages/productInfo/ProductInfo";
+import ScrollTop from "./components/scrollTop/ScrollTop";
+import CartPage from "./components/cart/CartPage"
+import AllProduct from "./pages/allProduct/AllProduct";
+import Login from "./pages/registration/Login";
+import Signup from "./pages/registration/Signup";
+import UserDashboard from "./pages/user/UserDashboard";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AddProductPage from "./pages/admin/AddProductPage";
+import UpdateProductPage from "./pages/admin/UpdateProductPage";
+import MyState from "./context/myState";
+import { Toaster } from "react-hot-toast";
+import ProtectedRouteForUser from "./protectedRoute/ProtectedRouteForUser";
+import ProtectedRouteForAdmin from "./protectedRoute/ProtectedRouteForAdmin";
+import CategoryPage from "./pages/category/CategoryPage"
+import Contact from "./pages/contact/contact"
+
 function App () {
   return (
     <MyState>
